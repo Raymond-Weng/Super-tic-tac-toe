4a22f39129de5f0c27e14ec8685a28aec4259a0b
Raymond <yuhongweng0501@gmail.com>
2023-12-17 14:53:02 +0800

    Update

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,10 +1,17 @@
+import sun.awt.image.GifImageDecoder;
+
 import javax.swing.*;
 import javax.swing.border.LineBorder;
+import javax.swing.plaf.metal.MetalCheckBoxIcon;
+import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
 public class Main {
     public static Main main;
+
+    JFrame startUp;
+    JTable jTable;
 
     JFrame jFrame;
     JPanel[][] panels;
@@ -15,20 +22,58 @@ public class Main {
     boolean ended;
 
     public static void main(String[] args) {
-        System.out.println("Game start");
         main = new Main();
-        main.start();
-        main.player = "O";
-        main.ended = false;
-        main.done = new boolean[3][3];
-        for (int x = 0; x < 3; x++) {
-            for (int y = 0; y < 3; y++) {
-                main.done[x][y] = false;
-            }
-        }
+        main.setup();
     }
 
-    public void start() {
+    public void setup(){
+        startUp = new JFrame();
+        startUp.setLayout(new GridLayout(4,2));
+
+        startUp.getContentPane().add(new JLabel());
+        startUp.getContentPane().add(new JLabel("Super tic tac toe"));
+        startUp.getContentPane().getComponent(1).setFont(new Font(null, Font.PLAIN, 30));
+        startUp.getContentPane().add(new JCheckBox());
+        ((JCheckBox)startUp.getContentPane().getComponent(2)).setIcon (new MetalCheckBoxIcon() {
+            protected int getControlSize() { return 30; }
+        });
+        startUp.getContentPane().add(new JLabel("O AI"));
+        startUp.getContentPane().getComponent(3).setFont(new Font(null, Font.PLAIN, 30));
+        startUp.getContentPane().add(new JCheckBox());
+        ((JCheckBox)startUp.getContentPane().getComponent(4)).setIcon (new MetalCheckBoxIcon() {
+            protected int getControlSize() { return 30; }
+        });
+        startUp.getContentPane().add(new JLabel("X AI"));
+        startUp.getContentPane().getComponent(5).setFont(new Font(null, Font.PLAIN, 30));
+        startUp.add(new JLabel());
+        JButton jButton = new JButton("start");
+        jButton.setFont(new Font(null, Font.PLAIN, 30));
+        jButton.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                main.player = "O";
+                main.ended = false;
+                main.done = new boolean[3][3];
+                for (int x = 0; x < 3; x++) {
+                    for (int y = 0; y < 3; y++) {
+                        main.done[x][y] = false;
+                    }
+                }
+                main.start(((JCheckBox)main.startUp.getContentPane().getComponent(2)).isSelected(),
+                        ((JCheckBox)main.startUp.getContentPane().getComponent(4)).isSelected());
+                main.startUp.dispose();
+            }
+        });
+        startUp.add(jButton);
+
+        startUp.setTitle("Super tic tac toe (Game  setting)");
+        startUp.pack();
+        startUp.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        startUp.setVisible(true);
+    }
+
+    public void start(boolean oAI, boolean xAI) {
+        System.out.println("Game start");
         jFrame = new JFrame();
         jFrame.setLayout(new GridLayout(3, 3));
 
@@ -164,6 +209,32 @@ public class Main {
                                     panels[bx][by].getComponent(0).setEnabled(false);
                                 }
 
+                                noSpace = true;
+                                for (int tx = 0; tx < 3; tx++) {
+                                    for (int ty = 0; ty < 3; ty++) {
+                                        if (!done[tx][ty]) {
+                                            noSpace = false;
+                                            break;
+                                        }
+                                    }
+                                    if (!noSpace) {
+                                        break;
+                                    }
+                                }
+                                if(!ended && noSpace){
+                                    ended = true;
+                                    for (int bx = 0; bx < 3; bx++) {
+                                        for (int by = 0; by < 3; by++) {
+                                            for (int x = 0; x < 3; x++) {
+                                                for (int y = 0; y < 3; y++) {
+                                                    main.buttons[bx][by][x][y].setEnabled(false);
+                                                }
+                                            }
+                                        }
+                                    }
+                                    JOptionPane.showMessageDialog(null, "Draw!", "Game ended", JOptionPane.INFORMATION_MESSAGE);
+                                }
+
                                 for (int tbx = 0; tbx < 3 && !ended; tbx++) {
                                     for (int tby = 0; tby < 3; tby++) {
                                         for (int tx = 0; tx < 3; tx++) {
@@ -175,6 +246,9 @@ public class Main {
                                 }
 
                                 player = (player.equals("O")) ? "X" : "O";
+                                if(!ended && ((player.equals("O") && oAI) || (player.equals("X") && xAI))){
+                                    ai();
+                                }
                             }
                         });
                     }
@@ -183,9 +257,29 @@ public class Main {
         }
 
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
         jFrame.setTitle("Super tic tac toe");
         jFrame.pack();
-        jFrame.setVisible(true);
+        if(oAI){
+            ai();
+        }
+    }
+
+    public void ai() {
+        boolean did = false;
+        while(!did) {
+            int bx = (int) (Math.random() * 3);
+            int by = (int) (Math.random() * 3);
+            if(done[bx][by]){
+                continue;
+            }
+            int x = (int) (Math.random() * 3);
+            int y = (int) (Math.random() * 3);
+            if(buttons[bx][by][x][y].isEnabled()){
+                buttons[bx][by][x][y].getActionListeners()[0].actionPerformed(null);
+                did = true;
+            }
+        }
     }
 
     public void clean(int x, int y) {
@@ -271,7 +365,6 @@ public class Main {
     }
 
     public void end() {
-        JOptionPane.showMessageDialog(null, player + " won!", "Game ended", JOptionPane.INFORMATION_MESSAGE);
         ended = true;
         for (int bx = 0; bx < 3; bx++) {
             for (int by = 0; by < 3; by++) {
@@ -282,5 +375,6 @@ public class Main {
                 }
             }
         }
+        JOptionPane.showMessageDialog(null, player + " won!", "Game ended", JOptionPane.INFORMATION_MESSAGE);
     }
 }
